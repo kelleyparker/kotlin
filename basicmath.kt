47c9dd2adadf628dfb32938e8217d3af6b997027
Kelley Parker <kelley.parker07@gmail.com>
2022-12-14 15:26:23 -0600

    Create basicmath.kt

---
+++ basicmath.kt
@@ -0,0 +1,27 @@
+fun add(n1: Int, n2: Int) {
+    println(n1+n2)
+}
+
+fun subtract(n1: Int, n2: Int) {
+    println(n1-n2)
+}
+
+fun multiply(n1: Int, n2: Int) {
+    println(n1*n2)
+}
+
+fun divide(n1: Int, n2: Int) {
+    println(n1/n2)
+}
+
+fun main() {
+	
+    val x = 3
+    val y = 4
+    
+    add(x,y)
+    subtract(x,y)
+    multiply(x,y)
+    divide(x,y)
+    
+}
